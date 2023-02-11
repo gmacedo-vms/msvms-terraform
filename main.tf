bad14d2fdc15153456dc723154a372797509354a
gmacedo-vms <g.macedochavarria@gmail.com>
2023-02-11 04:24:19 -0500

    Udemy-Terraform: Commands Basics

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+    required_version = ">= 1.0"
+    required_providers{
+        azurerm ={
+            source = "hashicorp/azurerm"
+            version = "2.67.0"
+        }
+    }
+}
+
+provider "azurerm"{
+    features{}
+}
+
+resource "azurerm_resource_group" "example"{
+    name = "example-resource-group"
+    location = "West Europe"
+}
+
+output "abc" {
+    value = azurerm_resource_group.example.connection
+}
